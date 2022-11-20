110af2f9a5abb4bfc2bd269d76763100f933a9e8
Антон Панов <a.panov@avtoexpress.org>
2022-11-21 00:44:36 +0300

    Пн 21 ноя 2022 00:44:36 MSK update

--- release.sh
+++ release.sh
@@ -2,8 +2,17 @@ nim c \
     --dynlibOverride:sqlite3 \
     --passL:"sqlite3.o -lm -pthread" \
     --passL:-static \
+    --spellSuggest \
     -d:release --opt:size \
     wapp_simple_nim_stats.nim
+
+if [ "$?" != "0" ]; then
+    echo "====================================================="
+    echo "ERROR"
+    echo
+    exit 1
+fi
+
 # nim c \
 #     --gcc.exe:musl-gcc \
 #     --gcc.linkerexe:musl-gcc \
@@ -12,11 +21,33 @@ nim c \
 #     -d:release --opt:size \
 #     wapp_simple_nim_stats.nim
 strip -s ./wapp_simple_nim_stats
+
+if [ "$?" != "0" ]; then
+    echo "====================================================="
+    echo "ERROR"
+    echo
+    exit 1
+fi
+
 upx --best ./wapp_simple_nim_stats
+
+if [ "$?" != "0" ]; then
+    echo "====================================================="
+    echo "ERROR"
+    echo
+    exit 1
+fi
 
 git add .
 git commit -am "`date` update"
 git push
+
+if [ "$?" != "0" ]; then
+    echo "====================================================="
+    echo "ERROR"
+    echo
+    exit 1
+fi
 
 timestamp=$(date +%s)
 VERSION=$(echo `cat VERSION`.$timestamp)
--- wapp_simple_nim_stats.nim
+++ wapp_simple_nim_stats.nim
@@ -1,18 +1,13 @@
 import asyncnet, asyncdispatch, httpcore, strutils, strformat, os
-import std/strformat
-import std/strtabs
 import std/json
 import std/jsonutils
 import std/envvars
-import strutils
 import norm/[model, sqlite, pragmas]
 import std/tables
-import times, os
-
+import times
 import fastkiss
-from fastkiss/utils import decodeData
-import regex
-from strformat import `&`
+import sugar
+import system/ansi_c
 
 putEnv("PORT", getEnv("PORT", "9000"))
 putEnv("DB_HOST", getEnv("DB_HOST", "./db.sqlite.db"))
@@ -37,67 +32,76 @@ proc fnGetEnv(): Table[string, string] {.inline.} =
 
     return aEnv
 
-proc fnAddRecord(req: Request): int = 
-    var aEnv = fnGetEnv()
-    var sEnv = $(aEnv.toJson)
-    echo "ENV: ", sEnv
+# discard await 
 
-    var iDateTime = now().utc
-    var sRemoteAddr = aEnv["REMOTE_ADDR"]
-    var oHTTPRequest = newHTTPRequest(iDateTime, sRemoteAddr, sEnv)
+proc getCounter(
+    req: Request,
+    db: DbConn): Future[system.void] {.async.} =
+    echo "REQUEST: ", req.reqMethod, " ", req.url
 
-    var db = getDb()
-    db.createTables(oHTTPRequest)
-    db.insert(oHTTPRequest)
+    req.response.headers["content-type"] = "image/svg+xml; charset=utf-8"
+    req.response.statusCode = Http200
 
-    var iC = db.count(HTTPRequest)
+    # var aEnv = fnGetEnv()
+    # var sEnv = $(aEnv.toJson)
+    # echo "ENV: ", sEnv
 
-    return iC
+    # var iDateTime = now().utc
+    # var sRemoteAddr = aEnv["REMOTE_ADDR"]
+    # var oHTTPRequest = newHTTPRequest(iDateTime, sRemoteAddr, sEnv)
+
+    # db.createTables(oHTTPRequest)
+    # db.insert(oHTTPRequest)
+
+    # var iC = db.count(HTTPRequest)
+    var iC = 0 
+
+    var sSVG = """
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="76" height="20" role="img" aria-label="statistics: {NUMBER}">
+<title>statistics: {NUMBER}</title>
+<linearGradient id="s" x2="0" y2="100%">
+    <stop offset="0" stop-color="#bbb" stop-opacity=".1" />
+    <stop offset="1" stop-opacity=".1" />
+</linearGradient>
+<clipPath id="r">
+    <rect width="76" height="20" rx="3" fill="#fff" />
+</clipPath>
+<g clip-path="url(#r)">
+    <rect width="59" height="20" fill="#555" />
+    <rect x="59" width="17" height="20" fill="#a4a61d" />
+    <rect width="76" height="20" fill="url(#s)" />
+</g>
+<g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
+    <text aria-hidden="true" x="305" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="490">statistics</text>
+    <text x="305" y="140" transform="scale(.1)" fill="#fff" textLength="490">statistics</text>
+    <text aria-hidden="true" x="665" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="70">1</text>
+    <text x="665" y="140" transform="scale(.1)" fill="#fff" textLength="70">{NUMBER}</text>
+</g>
+</svg>
+    """
+
+    sSVG = sSVG.replace("{NUMBER}", $iC)
+    sSVG.resp
 
 proc main() =
     let app = newApp()
     app.config.port = iPort
 
+    var db = getDb()
+
+    addSignal(SIGINT, proc(fd: AsyncFD): bool =
+        # asyncCheck 
+        close db
+        app.close()
+        echo "App shutdown completed! Bye-Bye Kisses :)"
+        quit(QuitSuccess)
+    )
+
     app.get("/testing", proc (req: Request) {.async.} =
-        "TESTING".respond
+        "TESTING".resp
     )
 
-    app.get("/", proc (req: Request) {.async.} =
-        echo "REQUEST: ", req.reqMethod, " ", req.url
-
-        req.response.headers["content-type"] = "image/svg+xml; charset=utf-8"
-        req.response.statusCode = Http200
-
-        var iC = fnAddRecord(req)
-
-        var sSVG = """
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="76" height="20" role="img" aria-label="statistics: {NUMBER}">
-    <title>statistics: {NUMBER}</title>
-    <linearGradient id="s" x2="0" y2="100%">
-        <stop offset="0" stop-color="#bbb" stop-opacity=".1" />
-        <stop offset="1" stop-opacity=".1" />
-    </linearGradient>
-    <clipPath id="r">
-        <rect width="76" height="20" rx="3" fill="#fff" />
-    </clipPath>
-    <g clip-path="url(#r)">
-        <rect width="59" height="20" fill="#555" />
-        <rect x="59" width="17" height="20" fill="#a4a61d" />
-        <rect width="76" height="20" fill="url(#s)" />
-    </g>
-    <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
-        <text aria-hidden="true" x="305" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="490">statistics</text>
-        <text x="305" y="140" transform="scale(.1)" fill="#fff" textLength="490">statistics</text>
-        <text aria-hidden="true" x="665" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="70">1</text>
-        <text x="665" y="140" transform="scale(.1)" fill="#fff" textLength="70">{NUMBER}</text>
-    </g>
-</svg>
-        """
-
-        sSVG = sSVG.replace("{NUMBER}", $iC)
-        # await respond sSVG
-        await req.respond(sSVG)
-    )
+    app.get("/", (req: Request) => req.getCounter(db))
 
     app.run()
 
