377bfeb71fc82c961b51ac6b264744ecb6c418a4
Антон Панов <a.panov@avtoexpress.org>
2022-11-20 16:01:42 +0300

    update

--- release.sh
+++ release.sh
@@ -1,4 +1,16 @@
-nim --gcc.exe:musl-gcc --gcc.linkerexe:musl-gcc --passL:-static -d:release --opt:size c wapp_simple_nim_stats.nim
+nim c \
+    --dynlibOverride:sqlite3 \
+    --passL:"sqlite3.o -lm -pthread" \
+    --passL:-static \
+    -d:release --opt:size \
+    wapp_simple_nim_stats.nim
+# nim c \
+#     --gcc.exe:musl-gcc \
+#     --gcc.linkerexe:musl-gcc \
+#     --passL:-static \
+#     --dynlibOverride:sqlite3 --passL:"sqlite3.o -lm -pthread" \
+#     -d:release --opt:size \
+#     wapp_simple_nim_stats.nim
 strip -s ./wapp_simple_nim_stats
 upx --best ./wapp_simple_nim_stats
 gh release create `cat VERSION` wapp_simple_nim_stats
